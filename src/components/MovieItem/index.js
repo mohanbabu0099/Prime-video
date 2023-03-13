2ebfacb05d71f4fa2356fb3606d757465091225e
mohan <mohanlucky52@gmail.com>
2023-03-13 06:55:57 +0000

    prime video

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,36 @@
+// Write your code here
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {details} = props
+  const {thumbnailUrl, videoUrl} = details
+  console.log(thumbnailUrl)
+  return (
+    <Popup
+      modal
+      trigger={<img src={thumbnailUrl} alt="thumbnail" />}
+      className="popup-content"
+    >
+      {close => (
+        <>
+          <div>
+            <ReactPlayer url={videoUrl} controls />
+          </div>
+          <button
+            data-testid="closeButton"
+            type="button"
+            className="trigger-button"
+            onClick={() => close()}
+          >
+            <IoMdClose />
+          </button>
+        </>
+      )}
+    </Popup>
+  )
+}
+export default MovieItem
---
+++ src/components/MoviesSlider/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import MovieItem from '../MovieItem'
+import './index.css'
+
+const MoviesSlider = props => {
+  const {details} = props
+
+  return (
+    <div className="sliderContainer">
+      <MovieItem details={details} />
+    </div>
+  )
+}
+export default MoviesSlider
